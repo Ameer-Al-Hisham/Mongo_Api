69c21726732760363a1701b4f24cdeae810e7a8c
Ameer-Al-Hisham <ameeralhisham007@gmail.com>
2023-09-12 17:03:42 +0530

    Extra functionalities added

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ client = pymongo.MongoClient(connection_string)
 
 db1 = client["database1"]
 collection1 = db1["collection1"]
-
+collection2 = db1["collection2"]
 
 # Flask
 app = Flask(__name__)
@@ -20,11 +20,11 @@ CORS(app)
 
 @app.route("/")
 def home():
-    return "Hello world Running"
+    return "Hello world"
 
 
-@app.route("/viewall", methods=["GET"])
-def viewall():
+@app.route("/viewusers", methods=["GET"])
+def viewusers():
     retdata = []
     data = collection1.find({},{"_id":0})
     for document in data:
@@ -32,14 +32,37 @@ def viewall():
       print(document)
     return jsonify(retdata)
 
-@app.route("/viewone", methods=["GET"])
-def viewone():
+@app.route("/findauser", methods=["GET"])
+def findauser():
   uid = request.args.get("uid")
   data = collection1.find_one({"Uid": uid}, {"_id": 0})
   return jsonify(data)
 
 
+@app.route("/usersignup",methods = ["POST"])
+def usersignup():
+  try:
+    data = request.get_json()
+    collection1.insert_one(data)
+    return "Success"
+  except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route("/getregcount", methods=["GET"])
+def getregcount():
+  data = collection1.count_documents({})
+  return jsonify(data)
+
+
+@app.route("/getalltask", methods=["GET"])
+def getalltask():
+  finlist = []
+  data = collection2.find({"Status":"active"},{"_id":0,"Description":0})
+  for doc in data:
+    finlist.append(doc)
+  return jsonify(finlist)
+  
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=False)
-
 
